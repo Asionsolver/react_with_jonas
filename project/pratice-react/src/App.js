9cef5ac5ddd474b585c63f064cfc4c69b69530fd
asionsolver <asionsolver274@gmail.com>
2023-08-24 23:45:05 +0600

    add new react project name like react-pratice

---
+++ project/pratice-react/src/App.js
@@ -0,0 +1,11 @@
+import Clock from './components/Clock';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1><Clock locale="bn-BD"/></h1>
+    </div>
+  );
+}
+
--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,20 +1,16 @@
 import {useState} from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Underwear", quantity: 12, packed: false },
-  { id: 4, description: "T-Shirts", quantity: 6, packed: true },
-  { id: 5, description: "Pants", quantity: 4, packed: true },
-  
-];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems}/>
+      <PackingList items={items}/>
       <Stats />
     </div>
   );
@@ -23,12 +19,10 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 💼</h1>;
 }
-function Form() {
+function Form({onAddItems}) {
   const[description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-
-
-
+  
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -39,6 +33,9 @@ function Form() {
       id: new Date().getTime(),
       packed: false};
       console.log(newItem);
+
+      onAddItems(newItem);
+
       setDescription("");
       setQuantity(1);
   }
@@ -58,11 +55,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({items}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id}/>
         ))}
       </ul>
