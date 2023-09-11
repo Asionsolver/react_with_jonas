79c7ceabab568ce92f153d674eaee8e66f0d8a67
asionsolver <asionsolver274@gmail.com>
2023-09-11 12:44:22 +0600

    add new project name like Password Generator

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,4 @@
-import {useState} from "react";
-
+import { useState } from "react";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -7,15 +6,26 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
-  function handleAddItem(id){
-    console.log(id);
+  function handleAddItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
   }
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={handleAddItems}/>
-      <PackingList items={items} onDeleteItem={handleAddItem}/>
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleAddItem}
+        onToggleItems={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -24,10 +34,11 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 💼</h1>;
 }
-function Form({onAddItems}) {
-  const[description, setDescription] = useState("");
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-  
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -36,49 +47,69 @@ function Form({onAddItems}) {
       description,
       quantity,
       id: new Date().getTime(),
-      packed: false};
-      console.log(newItem);
+      packed: false,
+    };
+    console.log(newItem);
 
-      onAddItems(newItem);
+    onAddItems(newItem);
 
-      setDescription("");
-      setQuantity(1);
+    setDescription("");
+    setQuantity(1);
   }
-
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for 😍 your trip?</h3>
-      <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((number) => (
-          <option value={number} key={number}>{number}</option>
+          <option value={number} key={number}>
+            {number}
+          </option>
         ))}
       </select>
-      <input type="text" placeholder="Item...." value={description} onChange={(e)=>setDescription(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button type="submit">Add</button>
     </form>
   );
 }
 
-function PackingList({items, onDeleteItem}) {
+function PackingList({ items, onDeleteItem, onToggleItems }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id}/>
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItems={onToggleItems}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItems }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration:'line-through'} : {}}>
-      {item.quantity} {item.description}
+      <input
+        type="checkbox"
+        checked={item.packed}
+        onChange={() => onToggleItems(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
       </span>
-      <button onClick={()=>onDeleteItem(item.id)}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
