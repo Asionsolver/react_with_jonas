445a6ad2d7e09a6f1c5e67578e16de706ffd378f
asionsolver <asionsolver274@gmail.com>
2023-10-01 22:10:33 +0600

    add new react mini-project name like flash-question-card

--- project/flash-question-card/src/App.js
+++ project/flash-question-card/src/App.js
@@ -1,66 +1,68 @@
-import "./styles.css";
 import { useState } from "react";
-
-export default function App() {
-  return (
-    <div className="App">
-      <FlashCards />
-    </div>
-  );
-}
+import "./App.css";
 
 const questions = [
   {
     id: 3457,
     question: "What language is React based on?",
-    answer: "JavaScript"
+    answer: "JavaScript",
   },
   {
     id: 7336,
     question: "What are the building blocks of React apps?",
-    answer: "Components"
+    answer: "Components",
   },
   {
     id: 8832,
     question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
+    answer: "JSX",
   },
   {
     id: 1297,
     question: "How to pass data from parent to child components?",
-    answer: "Props"
+    answer: "Props",
   },
   {
     id: 9103,
     question: "How to give components memory?",
-    answer: "useState hook"
+    answer: "useState hook",
   },
   {
     id: 2002,
     question:
       "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
+    answer: "Controlled element",
+  },
 ];
 
-function FlashCards() {
-  const [selectedId, setSelectedId] = useState(null);
-  function handleClicked(id) {
-    setSelectedId(id !== selectedId ? id : null);
-  }
+function App() {
   return (
-    <div className="flashcards">
-      {questions.map((question) => (
-        <div
-          key={question.id}
-          onClick={()=>handleClicked(question.id)}
-          className={question.id === selectedId ? "selected" : ""}
-        >
-          <p>
-            {question.id === selectedId ? question.answer : questions.question}
-          </p>
-        </div>
-      ))}
+    <div className="App">
+      <FlashCard />
     </div>
   );
 }
+
+function FlashCard() {
+  const[selectId, setSelectId] = useState(null);
+  function handleClick(id){
+    setSelectId(id!== selectId ? id : null);
+  }
+  return (
+    <div>
+      <div className="flashcards">
+        {questions.map((question) => (
+          <div 
+          key={question.id}
+          className={question.id === selectId ? 'selected' : ''}
+          onClick={()=>handleClick(question.id)}
+          >
+            <p>{question.id === selectId ? question.answer : question.question}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
