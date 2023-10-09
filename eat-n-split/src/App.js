05e35f411ea85310d3f187736cac1d100efdf810
asionsolver <asionsolver274@gmail.com>
2023-10-09 09:43:51 +0600

    add new md file name like 97 - Building the Static App Forms

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -24,7 +24,10 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend/>
       </div>
+
+      <FormSplitBill/>
     </div>
   );
 }
@@ -59,8 +62,50 @@ function Friends({ friend }) {
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({children}){
+  return(
+      <button className="button">{children}</button>
+  );
+}
+
+function FormAddFriend(){
+  return (
+      <form className='form-add-friend'>
+         <label>👨🏻‍🤝‍👩🏻Friend name</label>
+         <input type="text"/>
+
+         <label>🖼️ Image URL</label>
+         <input type="text"/>
+      </form>
+  )
+}
+
+function FormSplitBill(){
+  return(
+      <form className="form-split-bill">
+         <h2>Split a bill with X</h2>
+
+         <label>💰 Bill Value</label>
+         <input type="text"/>
+
+         <label>🧍Your expense</label>
+         <input type="text"/>
+
+         <label>👨🏻‍🤝‍👩🏻X's expense</label>
+         <input type="text" disabled/>
+
+         <label>🤑 Who is paying the bill</label>
+         <select>
+            <option value="user">You</option>
+            <option value="friend">X</option>
+         </select>
+         <Button>Split bill</Button>
+      </form>
   );
 }
 
