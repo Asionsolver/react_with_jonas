31f17a1cbc4e6a16308cfac5c62a36ceaed0ccd4
asionsolver <asionsolver274@gmail.com>
2023-11-07 12:22:39 +0600

    learn new topic name like React Composition vs Inheritance

--- project/pratice-react/src/App.js
+++ project/pratice-react/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
-
-import Home from './components/state_lifting/example_2/Home'
+import Text from "./components/inheritance/Text";
+import Emoji from "./components/inheritance/Emoji";
+import Bracket from "./components/inheritance/Bracket";
 
 export default function App() {
-    
   return (
     <div>
-      <Home/>
+      <Emoji>
+        {({ addEmoji }) => (
+          <Bracket>
+            {({ addBracket }) => (
+              <Text addEmoji={addEmoji} addBracket={addBracket} />
+            )}
+          </Bracket>
+        )}
+      </Emoji>
     </div>
-  )
+  );
 }
--- use-pop-corn/src/index.js
+++ use-pop-corn/src/index.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating'
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating'
 
-function Test(){
-  const [movieRating, setMovieRating] = useState(0);
-  return(
-    <div>
-      <StarRating maxRating={10} size={34} color='blue' onSetMovieRating={setMovieRating}/>
-      <p>This movies was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test(){
+//   const [movieRating, setMovieRating] = useState(0);
+//   return(
+//     <div>
+//       <StarRating maxRating={10} size={34} color='blue' onSetMovieRating={setMovieRating}/>
+//       <p>This movies was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating defaultRating={3}/>
+    <App />
+    {/* <StarRating defaultRating={3}/>
     <StarRating maxRating={5} size={24} color='blue' className='test' messages={['Terrible','Bad','Okay','Good','Amazing']}/>
-    <Test/>
+    <Test/> */}
   </React.StrictMode>
 );
 
